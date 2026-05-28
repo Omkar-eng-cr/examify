5797389f3fc83af2575d126f7cc65b0cfe6556b3
Omkar Chemate <omkarchemate45@gmail.com>
2026-05-28 16:05:46 +0530

    First Commit

---
+++ questions.js
@@ -0,0 +1,23 @@
+const questions = [
+
+    {
+        question: "What is HTML?",
+        options: ["Programming Language", "Markup Language", "Database", "Browser"],
+        answer: "Markup Language"
+    },
+
+    {
+        question: "What is CSS used for?",
+        options: ["Styling", "Database", "Server", "Compiler"],
+        answer: "Styling"
+    },
+
+    {
+        question: "What is JavaScript?",
+        options: ["Game", "Language", "Browser", "OS"],
+        answer: "Language"
+    }
+
+];
+
+module.exports = questions;
